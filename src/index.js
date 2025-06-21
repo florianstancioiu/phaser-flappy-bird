76fb2787dba9ca6299d710e0be850c83ed40e7ea
Florian Stancioiu <florinvictorstancioiu@gmail.com>
2025-06-21 13:46:09 +0300

    Gravity lesson

--- src/index.js
+++ src/index.js
@@ -15,6 +15,7 @@ const config = {
   scene: {
     preload: preload,
     create: create,
+    update: update,
   },
 };
 
@@ -22,8 +23,33 @@ new Phaser.Game(config);
 
 function preload() {
   this.load.image("sky", "assets/sky.png");
+  this.load.image("bird", "assets/bird.png");
 }
 
+let bird = null;
+let totalDelta = null;
+
 function create() {
-  this.add.image(400, 300, "sky");
+  this.add.image(config.width / 2, config.height / 2, "sky");
+  //debugger;
+  bird = this.physics.add
+    .sprite((1 / 10) * config.width, config.height / 2, "bird")
+    .setOrigin(0, 0);
+  // bird.body.gravity.y = 200;
+  // console.log(bird);
+}
+
+// 60fps
+// 60 times per second
+function update(time, delta) {
+  // console.log(bird.body.velocity.y);
+  // console.log(delta);
+  totalDelta += delta;
+
+  if (totalDelta < 1000) {
+    return;
+  }
+
+  console.log(bird.body.velocity.y);
+  totalDelta = 0;
 }
